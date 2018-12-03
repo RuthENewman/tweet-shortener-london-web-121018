992e919cee3b1ce1b87e5a31878285f6dfc7af75
ruthnewman <ruthnewman@gmail.com>
2018-12-03 16:10:37 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def dictionary = {
+  "hello" => "hi",
+  "to, two, too" => "2",
+  "for, four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+
+def word_substituter(string)
+  
+  
